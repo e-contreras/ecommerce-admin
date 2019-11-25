488ae751bc51dce10a0452f0908a83aee53bc2a7
imedina <imedina@consultoresinformaticos.com.py>
2019-11-25 16:55:35 -0300

    Assing packing

--- src/main/resources/static/js/logistics/assign_packing/assign-packing-index.js
+++ src/main/resources/static/js/logistics/assign_packing/assign-packing-index.js
@@ -1,13 +1,34 @@
 var vmAssignPacking = new Vue({
     el: '#assign-packing-index',
     data: {
-        listSolicitudes: []
+        listSolicitudes: [],
+        driverList:[],
+        valuePages: [],
+        vehiclesList:[],
+        itemPerPages: 3,
+        currentPage: 1,
+        modalMessage: "",
+        idAssingPacking: 0
+
     },
     created() {
+        this.getDriverAndVehicles();
         this.getSolicitudes();
     },
     computed: {
-        
+        getLength() {
+            return this.listSolicitudes.length;
+        },
+        countPages() {
+            return Math.ceil(this.getLength / this.itemPerPages);
+        },
+        pages() {
+            let pages = [];
+            for (let index = 0; index < this.countPages; index++) {
+                pages.push(index + 1);
+            }
+            return pages;
+        }
     },
     methods: {
         getSolicitudes(){
@@ -18,8 +39,7 @@ var vmAssignPacking = new Vue({
                     'Accept': 'application/json',
                 },
                 success: function (res) {
-                    this.listSolicitudes = res.data;
-                    console.log(listSolicitudes);
+                    this.listSolicitudes = res;
                     this.filterItems();
                 }.bind(this),
                 error: function (data) {
@@ -27,8 +47,104 @@ var vmAssignPacking = new Vue({
                 }.bind(this)
             });
         },
-        filterItems(){
-            console.log(this.listSolicitudes)
+        getDriverAndVehicles(){
+            $.ajax({
+                url: "http://localhost:8080/driver",
+                cache: false,
+                headers: {
+                    'Accept': 'application/json',
+                },
+                success: function (res) {
+                    this.driverList = res;
+                }.bind(this),
+                error: function (data) {
+                    console.log("error", data);
+                }.bind(this)
+            });
+            $.ajax({
+                url: "http://localhost:8080/vehicles",
+                cache: false,
+                headers: {
+                    'Accept': 'application/json',
+                },
+                success: function (res) {
+                    this.vehiclesList = res;
+                }.bind(this),
+                error: function (data) {
+                    console.log("error", data);
+                }.bind(this)
+            });
+        },
+        goToPage: function (page) {
+            this.currentPage = page;
+            this.filterItems();
+        },
+
+        filterItems: function () {
+            var indexTo = this.currentPage * this.itemPerPages;
+            var indexFrom = indexTo - this.itemPerPages;
+            this.valuePages = [];
+            for (var index = indexFrom; index < indexTo; index++) {
+                if (index < this.getLength) {
+                    if (this.listSolicitudes[index] !== undefined) {
+                        this.valuePages.push(this.listSolicitudes[index]);
+                    }
+                }
+            }
+        },
+        classState: function (state) {
+            switch (state.toUpperCase()) {
+                case "CERRADO":
+                    return "label-danger";
+                case "ABIERTO":
+                    return "label-primary";
+                case "CREADO":
+                    return "label-info";
+                case "FINALIZADO":
+                    return "label-success";
+                default:
+                    return "";
+            }
+        },
+        toDate(date){
+            let dateOld = new Date(date);
+            const separate = "-";
+            const space = " ";
+            let day;
+            let month;
+            let year;
+            let time = new Date().toLocaleTimeString();
+            if(dateOld === undefined){
+            	dateOld = new Date();
+        	}
+            if (dateOld instanceof Date) {
+                day = dateOld.getDate().toString();
+                month = (dateOld.getMonth() + 1).toString();
+                year = dateOld.getFullYear().toString();
+            } else {
+
+                let date = dateOld.toLocaleString().split("/");
+                day = date[2];
+                month = date[1];
+                year = date[0];
+            }
+            return year.concat(separate).concat(month).concat(separate).concat(day).concat(space).concat(time);
+        },
+
+        remove() {
+            let uri = "http://localhost:8080/solicitude/" + this.idAssingPacking;
+            $.ajax({
+                type: 'DELETE',
+                url: uri,
+                success: function (res) {
+                    window.setTimeout(function () {
+                        window.location.href = "./assign-packing";
+                    }, 1000);
+                }.bind(this),
+                error: function (data) {
+                    console.log("error", data);
+                }.bind(this)
+            });
         }
     }
 });
