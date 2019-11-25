7f1c8d63e3f45bc1dd1d4da5dd544c78b4b678b6
Gino Junchaya <gjunchaya@consultoresinformaticos.com.py>
2019-11-25 13:02:43 -0300

    se toco el javascript de empaque

--- src/main/resources/static/js/logistics/assign_packing/assign-packing-index.js
+++ src/main/resources/static/js/logistics/assign_packing/assign-packing-index.js
@@ -1,30 +1,34 @@
-var list = [{quality: 1, product: "Tv LG 32 LED HD", cod: 10001, description: "TV de 32 pulgadas"},
-    {quality: 1, product: "Play Station 5", cod: 100012, description: "Consola Play Station 5 - 2 Tb"}]
-
 var vmAssignPacking = new Vue({
     el: '#assign-packing-index',
     data: {
-        forClient: {
-            name: 'Helena Wayne',
-            address: 'Féliz Pérez Cardozo 123',
-            street: 'Lambaré, San Isidro',
-            telephone: '(555) 539-1037',
-            email: 'helena.wayne@batman.com'
-        },
-        pendingInvoice: {
-            id: '4F3S8J',
-            paymentDate: '2/22/2014',
-            account: '968-34567'
-        },
-        listProduct: list
+        listSolicitudes: []
     },
     created() {
-
+        this.getSolicitudes();
     },
     computed: {
-
+        
     },
     methods: {
-
+        getSolicitudes(){
+            $.ajax({
+                url: "http://localhost:8080/solicitude",
+                cache: false,
+                headers: {
+                    'Accept': 'application/json',
+                },
+                success: function (res) {
+                    this.listSolicitudes = res.data;
+                    console.log(listSolicitudes);
+                    this.filterItems();
+                }.bind(this),
+                error: function (data) {
+                    console.log("error", data);
+                }.bind(this)
+            });
+        },
+        filterItems(){
+            console.log(this.listSolicitudes)
+        }
     }
 });
